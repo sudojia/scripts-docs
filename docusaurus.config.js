46dfed58bc476dacaff8623424d15f8eba9fa2ba
sudojia <i@imzjw.cn>
2024-07-26 13:04:28 +0800

    chore:

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,15 +47,13 @@ const config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/sudojia/scripts-docs/tree/master/packages/create-docusaurus/templates/shared/',
+                        'https://github.com/sudojia/scripts-docs/tree/master/',
                 },
-                blog: {
-                    showReadingTime: true,
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-                },
+                // blog: {
+                //     showReadingTime: true,
+                //     editUrl:
+                //         'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                // },
                 theme: {
                     customCss: './src/css/custom.css',
                 },
